53abd5af44c5ccf959f8c75f18fd49b671dbd860
VJ-E <kit27.am57@gmail.com>
2025-09-16 14:17:47 +0530

    db connection logic implemented

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,7 +1,19 @@
 package com.todo;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.todo.util.DatabaseConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        DatabaseConnection db_connection = new DatabaseConnection();
+        try{
+            Connection cn = db_connection.getDBConnection();
+            System.out.println("Database connection has been established");
+        }
+        catch(SQLException e){
+            System.out.println("Database connection has failed");
+        }
     }
 }
---
+++ src/main/java/com/todo/util/DatabaseConnection.java
@@ -0,0 +1,27 @@
+package com.todo.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+
+public class DatabaseConnection {
+    public static final String URL = "jdbc:mysql://localhost:3306/todo";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = Dotenv.load().get("DB_PASSWORD");
+
+    static {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("JDBC Driver is missing");
+        }
+    }
+
+    public Connection getDBConnection() throws SQLException{
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
